b5b2fc59b87acdcbe809191837b614b3d07c8863
srikanth-kanakaboina <srikanth.kanakaboina@gmail.com>
2017-03-13 01:23:02 +0530

    Add class

---
+++ src/main/java/org/srikanth/controller/HomeController.java
@@ -0,0 +1,15 @@
+package org.srikanth.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+	
+	@RequestMapping("/hello")
+	public String sayHi() {
+		return "Hi";
+		
+	}
+
+}
---
+++ src/main/java/org/srikanth/srpingbootstarter/CourseAPIStarter.java
@@ -0,0 +1,14 @@
+package org.srikanth.srpingbootstarter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseAPIStarter {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CourseAPIStarter.class, args);
+
+	}
+
+}
